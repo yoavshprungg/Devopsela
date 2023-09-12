924405b7255606bf932958f5278998085743e1d4
yoavshprungg <142012587+yoavshprungg@users.noreply.github.com>
2023-09-12 09:54:33 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -2,7 +2,7 @@
 
 responseCode=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:5000)
 
-if [[ "$responseCode" -eq 200 ]]; then
+if [ "$responseCode" -eq 200 ]; then
     echo "Application test succeeded with response code 200"
 else
     echo "Application test failed with response code $responseCode"
